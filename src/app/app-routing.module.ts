36e8a723ccd42b0c05c2043c05d4a17eb6b236a1
Dejw7n <dejw7n@gmail.com>
2022-06-06 12:23:48 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./modules/auth/Guard/auth.guard";
+import { LoginComponent } from "./modules/auth/pages/login/login.component";
+import { DashboardComponent } from "./modules/client-area/pages/dashboard/dashboard.component";
+import { HomeComponent } from "./modules/main/pages/home/home.component";
+import { FilesComponent } from "./modules/panel/modules/server/pages/files/files.component";
+import { ServerComponent } from "./modules/panel/modules/server/pages/server/server.component";
+import { ServerListComponent } from "./modules/panel/pages/server-list/server-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: "", component: HomeComponent },
+	{ path: "login", component: LoginComponent },
+	{ path: "dashboard", component: DashboardComponent, canActivate: [AuthGuard] },
+	{ path: "servers", component: ServerListComponent, canActivate: [AuthGuard] },
+	{ path: "server/:id", component: ServerComponent, canActivate: [AuthGuard] },
+	{ path: "server/:id/files", component: FilesComponent, canActivate: [AuthGuard] },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AuthGuard } from "./modules/auth/Guard/auth.guard";
+import { TokenInterceptor } from "./modules/auth/Interceptor/token.interceptor";
+import { LoginComponent } from "./modules/auth/pages/login/login.component";
+import { ClientAreaModule } from "./modules/client-area/client-area.module";
+import { MainModule } from "./modules/main/main.module";
+import { PanelModule } from "./modules/panel/panel.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, LoginComponent],
+	imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, MainModule, ClientAreaModule, PanelModule],
+	providers: [
+		AuthGuard,
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: TokenInterceptor,
+			multi: true,
+		},
+	],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
